33a457515e8158dfc1c5612b751c191363047c88
Prathap011 <vjprathap35@gmail.com>
2024-06-12 12:23:51 +0530

    new files

--- data/view/turfAdminLoginView.py
+++ data/view/turfAdminLoginView.py
@@ -1,7 +1,7 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
-from ..model.turfRegisterModel import userLoginModel
+# from ..model.turfAminLoginModel import turfAdminLoginModel
 from data import message,jwtToken
 
 @csrf_exempt
@@ -12,14 +12,14 @@ def turfAdminLogin(request):
     password = data.get('password')
     print(email,password)
     try:
-      userId, role, mobileNumber = turfAdminLoginModel(mobileNumber)
+      # userId, role, mobileNumber = turfAdminLoginModel(mobileNumber)
       # Generate JWT token
-      jwtTokenEn = jwtToken.jwtTokenEncode(userId, role, mobileNumber)
-      print("User Login jwtTokenEn ------>",jwtTokenEn)
-      if userId is not None:
-        return message.response('Success','login',jwtTokenEn)  
-      else:
-        return message.response('Error','loginError',jwtTokenEn)
+      # jwtTokenEn = jwtToken.jwtTokenEncode(userId, role, mobileNumber)
+      print("User Login jwtTokenEn ------>")
+      # if userId is not None:
+      #   return message.response('Success','login',jwtTokenEn)  
+      # else:
+      #   return message.response('Error','loginError',jwtTokenEn)
     except Exception as e:
       print(str(e))
   return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
